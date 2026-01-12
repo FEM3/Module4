4d2e2e2cd4217bfca645456c2454fa13d4527d06
FEM3 <franklinemcgowaniii@gmail.com>
2026-01-11 19:30:43 -0700

    Create Cylinder.java

---
+++ Cylinder.java
@@ -0,0 +1,21 @@
+public class Cylinder extends Shape {
+    private double radius;
+    private double height;
+    
+    public Cylinder(double radius, double height) {
+        this.radius = radius;
+        this.height = height;
+    }
+    
+    public double surface_area() {
+        return 2 * Math.PI * radius * (radius + height);
+    }
+    
+    public double volume() {
+        return Math.PI * radius * radius * height;
+    }
+    
+    public String toString() {
+        return "Cylinder - Surface Area: " + surface_area() + ", Volume: " + volume();
+    }
+}
